a4c9a19832d61ea75638348b58233362cd340bd4
Rahul Chauhan <rahulchauhan4708@gmail.com>
2023-08-15 10:37:12 +0530

    added in new code

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import pymongo
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
-# @cross_origin
+@cross_origin
 def homepage():
     return render_template("index.html")
 
 @app.route("/review" , methods = ['POST' , 'GET'])
-# @cross_origin
+@cross_origin
 def index():
     if request.method == 'POST':
         try:
